c7fe4f6477c6255af552717a959dc720d3c68bbc
tobias-fyi <tobyreaper@gmail.com>
2020-02-20 12:12:47 -0700

    Added Material data insert to cli via  and to pytest via test_database fixture

--- detect/tests/conftest.py
+++ detect/tests/conftest.py
@@ -2,9 +2,12 @@
 TPDS Detect API :: Pytest Fixtures
 """
 
+import csv
+
 import pytest
 
 from detect import create_app, db
+from detect.api.models import Material
 
 
 @pytest.fixture(scope="module")
@@ -18,6 +21,17 @@ def test_app():
 @pytest.fixture(scope="module")
 def test_database():
     db.create_all()
+    # Add data to db
+    with open("detect/db/materials.csv", "r") as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            material = Material(
+                material_id=int(row[0]), description=row[1], cluster=row[2],
+            )
+            db.session.add(material)
+    db.session.commit()  # Commit the new changes
+
     yield db  # Testing happens here
+
     db.session.remove()
     db.drop_all()
--- manage.py
+++ manage.py
@@ -2,6 +2,7 @@
 TPDS Detect API :: Flask CLI Configuration
 """
 
+import csv
 import sys
 
 from flask.cli import FlaskGroup
@@ -18,7 +19,16 @@ cli = FlaskGroup(create_app=create_app)
 def recreate_db():
     db.drop_all()
     db.create_all()
-    db.session.commit()
+    # Add data to db
+    with open("detect/db/materials.csv", "r") as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            material = Material(
+                material_id=int(row[0]), description=row[1], cluster=row[2],
+            )
+            db.session.add(material)
+
+    db.session.commit()  # Commit the new changes
 
 
 if __name__ == "__main__":
