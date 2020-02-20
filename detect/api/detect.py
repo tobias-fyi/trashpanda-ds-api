e275f6fc61bdafac0dfc40c52c88857311bfeb29
tobias-fyi <tobyreaper@gmail.com>
2020-02-20 01:14:35 -0700

    Built out dummy detect endpoint and tests

--- detect/api/detect.py
+++ detect/api/detect.py
@@ -2,20 +2,67 @@
 TDPS Detect API :: Object Detection Endpoint
 """
 
-from flask import Blueprint
+import base64
+import binascii
+import json
+import io
+
+from flask import Blueprint, request
 from flask_restplus import Resource, Api
+from imageio import imread
 
 detect_blueprint = Blueprint("detect", __name__)
 api = Api(detect_blueprint)
 
 
+def from_base64(img_string: str):
+    """Converts a base64 image string to numpy uint8 image array."""
+    # If base64 has metadata attached, get only data after comma
+    if img_string.startswith("data"):
+        img_string = img_string.split(",")[-1]
+
+    # imageio array is a numpy array
+    return imread(io.BytesIO(base64.b64decode(img_string)))
+
+
 class Detect(Resource):
-    def get(self):
-        return {
-            "status": "success",
-            "cluster": "plastic_containers",
-            "materials": [593, 466, 621, 471, 236, 677,],
+    def post(self):
+        post_data = request.get_json()
+        img_b64 = post_data.get("imgb64")
+        response_object = {}
+
+        # Convert to imageio (numpy) array
+        try:
+            img = from_base64(img_b64)
+        except AttributeError:
+            response_object["message"] = "Input payload validation failed"
+            return response_object, 400
+        except binascii.Error as e:
+            response_object["message"] = str(e)
+            return response_object, 400
+
+        # TODO: Make prediction
+        predicted_cluster = "plastic_containers"
+
+        # TODO: Get material_id list from database
+        materials = [
+            593,
+            466,
+            621,
+            471,
+            677,
+        ]
+
+        # Success or not (not really necessary)
+        status = "success" if predicted_cluster else "fail"
+
+        response_object = {
+            "status": status,
+            "cluster": predicted_cluster,
+            "materials": materials,
         }
+
+        return response_object, 201
 
 
 api.add_resource(Detect, "/detect")
--- detect/tests/test_detect.py
+++ detect/tests/test_detect.py
@@ -4,15 +4,59 @@ TPDS Detect API :: Endpoint Test `/detect`
 
 import json
 
+# Load base64 string from file
+test_b64 = "detect/tests/images/img_b64.txt"
 
-def test_detect(test_app):
-    """Tests the `/detect` API endpoint."""
+with open(test_b64, "r") as f:
+    img_string = f.read()
+
+
+def test_detect(test_app, test_database):
+    """Tests the `/detect` API endpoint with base64 string."""
     # Make request and parse the response
     client = test_app.test_client()
-    resp = client.get("/detect")
+    resp = client.post(
+        "/detect",
+        data=json.dumps({"imgb64": img_string}),
+        content_type="application/json",
+    )
     data = json.loads(resp.data.decode())
     # Test the response
-    assert resp.status_code == 200
-    assert "plastic_containers" in data["cluster"]
+    assert resp.status_code == 201
     assert "success" in data["status"]
-    assert len(data["materials"]) > 0
+
+
+def test_detect_invalid_json(test_app, test_database):
+    """Test case for invalid json request."""
+    client = test_app.test_client()
+    resp = client.post("detect", data=json.dumps({}), content_type="application/json",)
+    data = json.loads(resp.data.decode())
+    assert resp.status_code == 400
+    assert "Input payload validation failed" in data["message"]
+
+
+def test_detect_invalid_json_keys(test_app, test_database):
+    """Test case for invalid json key in request."""
+    client = test_app.test_client()
+    resp = client.post(
+        "detect",
+        data=json.dumps({"b64_img": img_string}),
+        content_type="application/json",
+    )
+    data = json.loads(resp.data.decode())
+    assert resp.status_code == 400
+    assert "Input payload validation failed" in data["message"]
+
+
+def test_invalid_base64_string(test_app, test_database):
+    """Test case for invalid base64 image string in request."""
+    client = test_app.test_client()
+    resp = client.post(
+        "detect",
+        data=json.dumps({"imgb64": "img_string"}),
+        content_type="application/json",
+    )
+    data = json.loads(resp.data.decode())
+    assert resp.status_code == 400
+    assert "Invalid base64-encoded string" in data["message"]
+
